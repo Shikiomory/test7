3e426f17d8777d07d53daf011d61be25f61b9321
Shikomory <NeXoZplay@yandex.ru>
2024-09-21 16:32:08 +0300

    fourth

--- stringclass.cpp
+++ stringclass.cpp
@@ -128,7 +128,7 @@ int main() {
     res.print();
     cout << '\n' << (str == res) << endl;
     cout << str[1] << endl;
-    cout << "hell" << endl;
-    cout << "sos";
+    cout << "world" << endl;
+    cout << "hell";
     return 0;
 }
